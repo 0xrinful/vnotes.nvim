389880284a2ed1ffb93951168976c9c2af09fe04
0xrinful <ahmedxanon012@gmail.com>
2025-05-16 14:20:25 +0300

    remove print statements

--- lua/vnotes/init.lua
+++ lua/vnotes/init.lua
@@ -4,21 +4,19 @@ local config = require("vnotes.config")
 local M = {}
 
 M.setup = function(opts)
-  print(vim.inspect(config.options))
-  config.setup(opts or {})
-  print(vim.inspect(config.options))
+	config.setup(opts or {})
 
-  vim.api.nvim_create_user_command("VNoteCreate", function()
-    core.create_note()
-  end, {})
+	vim.api.nvim_create_user_command("VNoteCreate", function()
+		core.create_note()
+	end, {})
 
-  vim.api.nvim_create_user_command("VNoteOpen", function()
-    core.open_note()
-  end, {})
+	vim.api.nvim_create_user_command("VNoteOpen", function()
+		core.open_note()
+	end, {})
 
-  vim.api.nvim_create_user_command("VNoteSelect", function()
-    core.select_note()
-  end, {})
+	vim.api.nvim_create_user_command("VNoteSelect", function()
+		core.select_note()
+	end, {})
 end
 
 return M
